640a180b6794236ab53e06b6d1491f29ae49c22f
LuvioBlaze <goodluck8585.com@gmail.com>
2026-07-30 12:27:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -188,3 +188,21 @@ function acceptCookies() {
 function declineCookies() {
   document.getElementById("cookie-banner").style.display = "none";
 }
+// Back To Top Button
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
